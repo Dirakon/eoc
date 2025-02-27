411fdcb560230b7a55c234b67b9641d06f58fb7e
Dirakon <609700zxc@mail.ru>
2025-02-28 01:32:26 +0300

    Revert "Change script for huggingface"
    This reverts commit 6458c9c3cd6f49d6cf3ede84e4fa2e3bda0eed62.

--- generate_docs.sh
+++ generate_docs.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-# MODELS=("llama3.2:3b" "deepseek-r1:8b")
-MODELS=("deepseek-ai/DeepSeek-R1")
+# OLLAMA_MODELS=("llama3.2:3b" "deepseek-r1:8b")
+OLLAMA_MODELS=("deepseek-r1:1.5b")
 
 # Find all subdirectories in runs_temp
 find ./runs_temp -mindepth 1 -maxdepth 1 -type d | while read -r dir; do
     # Process each model for the current directory
-    for model in "${MODELS[@]}"; do
+    for model in "${OLLAMA_MODELS[@]}"; do
 	# Sanitize model name for filename by replacing ':' with '-'
 	safe_model="${model//:/-}"
 	
@@ -18,14 +18,14 @@ find ./runs_temp -mindepth 1 -maxdepth 1 -type d | while read -r dir; do
 	if [ -f "$prompt_template" ]; then
 	    echo "Processing $(basename "$dir") with model $model"
 	    node ./src/eoc.js generate_comments \
-		--provider huggingface \
-		--huggingface_model "$model" \
-		--huggingface_token "$HUGGINGFACEHUB_API_KEY" \
+		--provider ollama \
+		--ctx_size 8000 \
+		--ollama_model "$model" \
 		--source app.eo \
 		--output "$output_file" \
 		--prompt_template "$prompt_template"
 	    # Cleanup
-	    # ollama stop "$model"
+	    ollama stop "$model"
 	else
 	    echo "Warning: Missing prompt template in ${dir}"
 	fi
